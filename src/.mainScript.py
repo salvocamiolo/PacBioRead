e024729c959e7d114e33a8e787467d57afca6cc1
salvocamiolo <salvocamiolo@gmail.com>
2020-06-06 19:28:08 +0100

    added gap closing

--- src/.mainScript.py
+++ src/.mainScript.py
@@ -440,7 +440,7 @@ class Ui_Form(object):
 							record.letter_annotations["phred_quality"] = [40] * len(record)
 							SeqIO.write(record, fastq, "fastq")
 					os.system(installationDirectory+"/src/conda/bin/bowtie2-build "+reference+" bowtie2Ref")
-					os.system(installationDirectory+"/src/conda/bin/bowtie2 -U "+self.readsFileLineEdit.text()+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
+					os.system(installationDirectory+"/src/conda/bin/bowtie2 --very-sensitive-local  -U "+self.readsFileLineEdit.text()+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
 					#os.system(installationDirectory+"/src/conda/bin/bowtie2 -U "+reads+".fastq "+" "+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
 					os.system(installationDirectory+"/src/conda/bin/samtools view -F 4 -bS -h "+outputFolder+"/bowtie2Alignment.sam > "+outputFolder+"/bowtie2Mapped.bam")
 					os.system(installationDirectory+"/src/conda/bin/samtools sort -o "+outputFolder+"/bowtie2Mapped_sorted.bam "+outputFolder+"/bowtie2Mapped.bam") 
