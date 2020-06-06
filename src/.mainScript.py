f230a7746c6bbb008ca7e2898969483f736f96a8
salvocamiolo <salvocamiolo@gmail.com>
2020-06-06 18:08:17 +0100

    added gap closing

--- src/.mainScript.py
+++ src/.mainScript.py
@@ -542,6 +542,7 @@ class Ui_Form(object):
 
 							
 							os.system(installationDirectory+"/src/conda/bin/cap3 "+outputFolder+"/preliminaryContigs.fasta")
+							os.system(installationDirectory+"/src/conda2/bin/ragout -o "+outputFolder+"/ragoutOutput_afterGapClosing "+outputFolder+"/ragout_recepie.rcp")
 							
 
 
