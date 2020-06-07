9ffaf8e5efcca0ef176764ad102f1dc497037032
salvocamiolo <salvocamiolo@gmail.com>
2020-06-07 16:29:08 +0100

    added gap closing

--- src/.mainScript.py
+++ src/.mainScript.py
@@ -436,13 +436,13 @@ class Ui_Form(object):
 					self.logTextEdit.append("Aligning reads.... ")
 					self.logTextEdit.repaint()
 
-					with open(reads, "r") as fasta, open(reads+".fastq", "w") as fastq:
-						for record in SeqIO.parse(fasta, "fasta"):
-							record.letter_annotations["phred_quality"] = [40] * len(record)
-							SeqIO.write(record, fastq, "fastq")
+					#with open(reads, "r") as fasta, open(reads+".fastq", "w") as fastq:
+					#	for record in SeqIO.parse(fasta, "fasta"):
+					#		record.letter_annotations["phred_quality"] = [40] * len(record)
+					#		SeqIO.write(record, fastq, "fastq")
 					os.system(installationDirectory+"/src/conda/bin/bowtie2-build "+reference+" bowtie2Ref")
 					#os.system(installationDirectory+"/src/conda/bin/bowtie2 --very-sensitive-local -U "+self.readsFileLineEdit.text()+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
-					os.system(installationDirectory+"/src/conda/bin/bowtie2 --very-sensitive-local -U "+reads+".fastq "+" "+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
+					os.system(installationDirectory+"/src/conda/bin/bowtie2 --very-sensitive-local -U "+reads+" -x "+outputFolder+"/bowtie2Ref -S "+outputFolder+"/bowtie2Alignment.sam -p 8") #To add num threads
 					os.system(installationDirectory+"/src/conda/bin/samtools view -F 4 -bS -h "+outputFolder+"/bowtie2Alignment.sam > "+outputFolder+"/bowtie2Mapped.bam")
 					os.system(installationDirectory+"/src/conda/bin/samtools sort -o "+outputFolder+"/bowtie2Mapped_sorted.bam "+outputFolder+"/bowtie2Mapped.bam") 
 					os.system(installationDirectory+"/src/conda/bin/samtools index "+outputFolder+"/bowtie2Mapped_sorted.bam")
