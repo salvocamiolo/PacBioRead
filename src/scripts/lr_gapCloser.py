0424c215f2c7e905453325868ce7d0f6c7c4b413
salvocamiolo <salvocamiolo@gmail.com>
2020-06-08 19:37:34 +0100

    updated lr_gapcloser

--- src/scripts/lr_gapCloser.py
+++ src/scripts/lr_gapCloser.py
@@ -207,7 +207,8 @@ for a in range(len(scaffoldPositionOrdered)-1):
                 gapClosed = True
     
     outfile.write(">"+scaffoldName[scaffoldPosition.index(scaffoldPositionOrdered[a])]+"_elonged\n"+elongedSequence+"\n")
-    outfile.close()
+
+outfile.close()    
 os.system("cp "+sequenceOutputName+" "+outputFolder+"/")
 os.chdir("../")
 os.system("rm -rf "+randomFolderName)
