421dcbff17f617ae8e355a364cce009ef1ee97ec
salvocamiolo <salvocamiolo@gmail.com>
2020-06-10 12:34:46 +0100

    added contigsJoiner

--- src/scripts/contigsJoiner.py
+++ src/scripts/contigsJoiner.py
@@ -18,6 +18,7 @@ reference = args['reference']
 
 
 
+
 #Mapping the reads
 os.system(installationDirectory+"/src/conda/bin/lastz "+reference+" "+contigs+" --format=general > scaffoldMapping.txt")
 #Getting position and strand for best alignment
@@ -35,21 +36,35 @@ while True:
         scaffoldInfo[fields[6]] = [int(fields[0]),int(fields[4]),fields[7]]
 
 #GEnerating a new fasta file with the contigs in the correct orientation
+contigsSeq = {}
 outfile = open(contigs+"_oriented.fasta","w")
+for seq_record in SeqIO.parse(contigs,"fasta"):
+    if not str(seq_record.id) in contigsSeq:
+        if scaffoldInfo[str(seq_record.id)][2]=="+":    
+            contigsSeq[str(seq_record.id)] = str(seq_record.seq)
+        else:
+            contigsSeq[str(seq_record.id)] = Seq.reverse_complement(str(seq_record.seq))
+
+#Get the contigs names in the righ order
 infile = open(contigs)
+orderedContigs = []
 while True:
     title = infile.readline().rstrip()
     if not title:
         break
-    outfile.write(title+"\n")
     title = title.replace(">","")
-    sequence  = infile.readline().rstrip()
-    if scaffoldInfo[title][2] == "+":
-        print(title+" oriented forward")
-        outfile.write(sequence+"\n")
-    else:
-        print(title+" oriented reverse")
-        outfile.write(Seq.reverse_complement(sequence)+"\n")
+    orderedContigs.append(title)
+
+
+#Join adjacent contigs
+for a in range(len(orderedContigs)-1):
+    print("joining "+orderedContigs[a]+" and "+orderedContigs[a+1])
+    
+
+
+    
+
+
 
 
 
